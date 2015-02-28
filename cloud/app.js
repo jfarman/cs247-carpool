42a08357a9a9182d126e9bdfdebf392afe049ca3
Ricky Tran <rtran58@DN51ushj.SUNet>
2015-02-28 12:38:10 -0800

    initial skeleton code

---
+++ cloud/app.js
@@ -0,0 +1,116 @@
+
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
+
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'ejs');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+// This is an example of hooking up a request handler with a specific request
+// path and HTTP verb using the Express routing API.
+app.get('/', function(req, res) {
+  res.render('pages/index',
+      {
+        title: "Index"    
+      });
+});
+
+app.get('/login', function(req, res) {
+  res.render('pages/login',
+      {
+        title: "Login"    
+      });
+});
+
+app.get('/flot', function(req, res) {
+  res.render('pages/flot',
+      {
+        title: "Flot Charts"    
+      });
+});
+
+app.get('/morris', function(req, res) {
+  res.render('pages/morris',
+      {
+        title: "Morris Charts"    
+      });
+});
+
+app.get('/tables', function(req, res) {
+  res.render('pages/tables',
+      {
+        title: "Tables"    
+      });
+});
+
+app.get('/forms', function(req, res) {
+  res.render('pages/forms',
+      {
+        title: "Forms"    
+      });
+});
+
+app.get('/panels-wells', function(req, res) {
+  res.render('pages/panels-wells',
+      {
+        title: "Paensl and Wells"    
+      });
+});
+
+app.get('/buttons', function(req, res) {
+  res.render('pages/buttons',
+      {
+        title: "Buttons"    
+      });
+});
+
+app.get('/notifications', function(req, res) {
+  res.render('pages/notifications',
+      {
+        title: "Notifications"    
+      });
+});
+
+app.get('/typography', function(req, res) {
+  res.render('pages/typography',
+      {
+        title: "Typography"    
+      });
+});
+
+app.get('/icons', function(req, res) {
+  res.render('pages/icons',
+      {
+        title: "Icons"    
+      });
+});
+
+app.get('/grid', function(req, res) {
+  res.render('pages/grid',
+      {
+        title: "Grid"    
+      });
+});
+
+app.get('/blank', function(req, res) {
+  res.render('pages/blank',
+      {
+        title: "Blank Page"    
+      });
+});
+// // Example reading from the request query string of an HTTP get request.
+// app.get('/test', function(req, res) {
+//   // GET http://example.parseapp.com/test?message=hello
+//   res.send(req.query.message);
+// });
+
+// // Example reading from the request body of an HTTP post request.
+// app.post('/test', function(req, res) {
+//   // POST http://example.parseapp.com/test (with request body "message=hello")
+//   res.send(req.body.message);
+// });
+
+// Attach the Express app to Cloud Code.
+app.listen();
